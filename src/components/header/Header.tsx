5d817ad8bdd945e856850fcc50cf4ab895236747
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-19 18:21:48 -0300

    feat: Add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderContainer, HeaderTitle } from "./Styles";
+
+export const Header: React.FC = () => {
+    return (
+        <HeaderContainer>
+            <HeaderTitle>measure converter</HeaderTitle>
+        </HeaderContainer>
+    )
+}
